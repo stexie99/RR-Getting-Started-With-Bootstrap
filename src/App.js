3a32d580e91fa5e94d1e7df73655613a000335c9
Steven Xie <xies1999@hotmail.com>
2024-02-07 14:40:22 -0500

    implemented bootstrap form, nav, and buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
 import './App.css';
-
-
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/nav'
+import Form from 'react-bootstrap/Form'
 
 function App() {
   return (
     <div className="App">
+      <Nav activeKey='/home'>
+        <Nav.Item>
+          <Nav.Link href='/home'>Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey='/about'>About Us</Nav.Link>
+        </Nav.Item>
+      </Nav>
       <h1>Bootstrap Website</h1>
-     
+      <Button variant="success">Success</Button>{' '}
+      <Form>
+        <Form.Group controlId='formBasicEmail'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control type='email' placeholder='Enter Email' />
+          <Form.Text className='text-muted'>
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group controlId='formBasicPassword'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control type='password' placeholder='Password' />
+        </Form.Group>
+        <Form.Group controlId='formBasicCheckbox'>
+          <Form.Check type='checkbox' label='Check me out'  />
+        </Form.Group>
+        <Button variant='primary' type='submit'>
+          Submit
+        </Button>
+        <Form.Group>
+          <Form.Label>Form Dropdown with favorite movies</Form.Label>
+          <Form.Control as="select">
+            <option>Inception</option>
+            <option>Forrest Gump</option>
+            <option>Shawshank Redemption</option>
+            <option>Spiderman: Into the Spiderverse</option>
+            <option>Avengers: Endgame</option>
+          </Form.Control>
+        </Form.Group>
+
+
+      </Form>
     </div>
   );
 }
